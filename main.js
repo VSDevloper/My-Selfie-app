647b67d6f6a9b8fb5d414bb1a9bb1791390329fd
VSDevloper <84179803+VSDevloper@users.noreply.github.com>
2021-07-29 19:36:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -43,3 +43,28 @@ function speak(){
         save();
     }, 5000);
 }
+
+ 
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpg',
+    jpeg_quality:90
+});
+
+function take_selfie()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+}
